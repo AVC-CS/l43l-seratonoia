a3628bc270a6eed7fcff98fe0219e5b4204186fb
seratonoia <hjizelle@gmail.com>
2024-10-07 01:03:16 -0700

    notes in program and added variable j for goign through number input to sum total

--- main.py
+++ main.py
@@ -2,22 +2,24 @@ def main():
     total = 0
 
     numbers = [0] * 5
-    for i in range(5):
+    for i in range(len(numbers)):
         numbers[i] = int(input('Enter a value: '))
-        total = total + numbers
-        i += 1
+    print(numbers)
+
+    for j in (numbers):
+        total += j
+    print (total)       
+        
     """
     ########################################
     Code Your Program here
     ########################################
     """
     # the list of number value is initialized to whatever * 5 to hold 5 inputs
-
-
+    # try using another variable to keep track of the numbers
     # total = sum(numbers)
-    print(total)
-    print(numbers)
-
+    # display the lis tof numbers first, then display the sum of them
+    # initialize adding to total = 0
     ########################################
     # Do not delete the return statement
     ########################################
