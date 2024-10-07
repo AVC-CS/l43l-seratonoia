459d8dffd4d30940d0e1b05c2638ca42f34e62e5
seratonoia <hjizelle@gmail.com>
2024-10-07 00:58:35 -0700

    notes for program

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ def main():
     total = 0
 
     numbers = [0] * 5
-    for i in range(len(numbers)):
+    for i in range(5):
         numbers[i] = int(input('Enter a value: '))
-
+        total = total + numbers
+        i += 1
     """
     ########################################
     Code Your Program here
     ########################################
     """
+    # the list of number value is initialized to whatever * 5 to hold 5 inputs
+
 
     # total = sum(numbers)
     print(total)
+    print(numbers)
 
     ########################################
     # Do not delete the return statement
